d7d9c0553ccee778309d383b756ea9c59cca327b
Venkat3699 <Ravindrareddy.mukka9@gmail.com>
2025-01-30 16:10:14 +0530

    updated file

--- null-local-exec.tf
+++ null-local-exec.tf
@@ -1,14 +1,14 @@
 resource "null_resource" "wait_for_ansible" { 
-  depends_on = [local_file.ansible-inventory-file]
+  depends_on = [local_file.ansible_inventory_file]
 
   provisioner "local-exec" {
     command = <<EOH
       sleep 20
-      ansible -i invfile pvt -m ping -vvv --extra-vars "ansible_python_interpreter=/usr/bin/python3" --ssh-extra-args="-o ControlMaster=no -o ServerAliveInterval=30 -o ServerAliveCountMax=5"
+      ansible -i ${local_file.ansible_inventory_file.filename} pvt -m ping -vvv --extra-vars "ansible_python_interpreter=/usr/bin/python3" --ssh-extra-args="-o ControlMaster=no -o ServerAliveInterval=30 -o ServerAliveCountMax=5"
     EOH
   }
 
   triggers = {
-    inventory_checksum = sha1(file("invfile"))
+    inventory_checksum = sha1(local_file.ansible_inventory_file.content)
   }
 }

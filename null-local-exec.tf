4a57165a1ae6d37f4092a150f2bd5556b86178e9
Venkat3699 <Ravindrareddy.mukka9@gmail.com>
2025-02-09 19:56:38 +0530

    updated files

--- null-local-exec.tf
+++ null-local-exec.tf
@@ -1,5 +1,10 @@
-resource "null_resource" "wait_for_ansible" { 
+resource "null_resource" "wait_for_ansible" {
   depends_on = [local_file.ansible-inventory-file]
+  triggers = {
+    time = "${timestamp()}"
+  }
+
+  count = length(aws_instance.webservers)
   provisioner "local-exec" {
     command = <<EOH
       sleep 20

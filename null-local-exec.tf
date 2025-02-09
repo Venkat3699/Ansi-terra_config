c1ba0fed16fc43aabc0fb8fe6c427dd21ed64b82
Venkat3699 <Ravindrareddy.mukka9@gmail.com>
2025-02-09 20:33:01 +0530

    updated files

--- null-local-exec.tf
+++ null-local-exec.tf
@@ -1,14 +1,17 @@
 resource "null_resource" "wait_for_ansible" {
-  depends_on = [local_file.ansible-inventory-file]
+  depends_on = [aws_instance.webservers]
   triggers = {
     time = "${timestamp()}"
   }
 
   count = length(aws_instance.webservers)
   provisioner "local-exec" {
-    command = <<EOH
-      sleep 20
-      ansible -i invfile pvt -m ping
-    EOH
+    command = "echo ${element(aws_instance.webservers[*].public_ip, count.index)} >> /etc/ansible/hosts"
+    when    = create
+  }
+
+  provisioner "local-exec" {
+    command = "rm -f /etc/ansible/hosts"
+    when    = destroy
   }
 }
